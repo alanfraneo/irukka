3b34065122d2d9926e372950a671d8e41160579d
Alan Francis <alanfraneo@gmail.com>
2021-09-30 14:57:36 +0530

    Updated - source URL params changed

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function() {
           pageUrl: {urlMatches: '.*'},
         })
         ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+            actions: [new chrome.declarativeContent.ShowAction()]
       }]);
     });
 });
--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ findPostsBtn.onclick = function(element) {
     var xhr = new XMLHttpRequest();
     let loader = document.getElementById("loader");
     loader.style.display = 'block';
-    xhr.open("GET", "https://pitstop.manageengine.com/portal/api/communityTopics?portalId=b87ca73bf299ade4c685d36796bf6574047c91cff3f9636f&from=1&limit=50&categoryId=49000000043983", true);
+    xhr.open("GET", "https://pitstop.manageengine.com/portal/api/communityTopics?portalId=edbsndda8617b893341bd1b77a449d99b88366b0a94f90974bdbdd9169acfd8f13c28&from=1&limit=50&categoryId=24000259278003", true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             // JSON.parse does not evaluate the attacker's scripts.
